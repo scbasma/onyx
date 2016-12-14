db023940041219ea21615d173c0f37c5b71bc0d2
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-12-14 15:02:24 -0800

    no done

--- test/onyx/scheduler/greedy_multi_job_test.clj
+++ test/onyx/scheduler/greedy_multi_job_test.clj
@@ -110,12 +110,10 @@
 
         replica-1 (playback-log (:log env) (extensions/subscribe-to-log (:log env) ch) ch 6000)
         counts-1 (get-counts replica-1 [j1 j2])
-        _ (>!! a-chan :done)
         _ (close! a-chan)
 
         replica-2 (playback-log (:log env) replica-1 ch 6000)
         counts-2 (get-counts replica-2 [j1 j2])
-        _ (>!! c-chan :done)
         _ (close! c-chan)
 
         replica-3 (playback-log (:log env) replica-2 ch 6000)
