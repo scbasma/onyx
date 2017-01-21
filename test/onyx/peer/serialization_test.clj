c15f517c69257fd69dabd548936dc711e2a8115d
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-01-21 02:20:53 -0800

    WIP

--- test/onyx/peer/serialization_test.clj
+++ test/onyx/peer/serialization_test.clj
@@ -5,21 +5,6 @@
             [org.agrona.concurrent UnsafeBuffer]
             [java.nio ByteBuffer]))
 
-; (time 
-;   (let [encoder (MapEncoder.)
-;         buffer (byte-array 900)
-;         buf ^UnsafeBuffer (UnsafeBuffer. buffer)]
-;     (dotimes [t 1000000] 
-;       (let [a "sietna"
-;             b " itnarinst"
-;             aa (.getBytes a)
-;             bb (.getBytes b)
-;             encoder (-> encoder 
-;                         (.wrap buf 0)
-;                         (.putKey aa 0 (alength aa))
-;                         (.putValue bb 0 (alength bb)))]
-;         (.encodedLength encoder)))))
-
 (let [decoders #^"[Lclojure.lang.IFn;" (make-array clojure.lang.IFn 30)]
   (aset decoders (MessageEncoder/TEMPLATE_ID) (fn [v] (MessageDecoder.)))
   decoders)
@@ -33,82 +18,37 @@
                        ;(.blockLength MessageEncoder/BLOCK_LENGTH)
                        ;(.templateId MessageEncoder/TEMPLATE_ID)
                        (.wrap buf 0)
-                       (.destId 68830003)
-                       (.replicaVersion 5000000000))
-           seg-count 20
-           seg-encoder (reduce (fn [^MessageEncoder$SegmentsEncoder enc v]
-                                 (let [bs ^bytes (messaging-compress {:n v :a "hiseta" :b "esntiarn"})
-                                       cnt ^int (alength bs)] 
-                                   (.putSegmentBytes (.next enc)
-                                                     bs
-                                                     0 
-                                                     cnt)))
-                               (.segmentsCount encoder seg-count)
-                               (range seg-count))]
+                       (.replicaVersion 5000000000)
+                       (.destId 6999))
+           seg-count 200
+           segments (map (fn [v]
+                           {:n v :a "hiseta" :b "esntiarn"})
+                         (range seg-count))
+           bs ^bytes (messaging-compress {:n 3 :a "isetna" :b "sietna"}) 
+           seg-encoder (loop [^MessageEncoder$SegmentsEncoder enc (.segmentsCount encoder seg-count)
+                              v (first segments) 
+                              vs (rest segments)]
+                         (let [;bs ^bytes (messaging-compress v) 
+                               cnt ^int (alength bs)]
+                           (when v 
+                             (recur (.putSegmentBytes (.next enc) bs 0 cnt)
+                                    (first vs) 
+                                    (rest vs)))))]
        (let [decoder (-> (MessageDecoder.)
                          ;; don't hardcode this thing
                          (.wrap buf 0 MessageDecoder/BLOCK_LENGTH 0))
+             replica-version (.replicaVersion decoder)
+             dest-id (.destId decoder)
              seg-dec (.segments decoder)
-             ;  #_(.next seg-dec)
-             messages (transient [])
-             ]
+             messages (transient [])]
          (loop [seg-dec seg-dec]
            (if (.hasNext seg-dec)
+             ;; can use an unsafe buffer here, but we have no way to read from it with nippy without
+             ;; copying to another byte array
              (let [bs (byte-array (.segmentBytesLength seg-dec))]
                (.getSegmentBytes seg-dec bs 0 (.segmentBytesLength seg-dec))
                (conj! messages (messaging-decompress bs))
                (recur (.next seg-dec)))))
-         (persistent! messages))))))
-
-(time (let [encoder ^MessageEncoder (MessageEncoder.)
-            buffer (byte-array 900)
-            buf ^UnsafeBuffer (UnsafeBuffer. buffer)]
-        (dotimes [v 1000000]
-          (let [encoder (-> encoder 
-                          (.wrap buf 0)
-                          (.replicaVersion 50000332)
-                          (.destId 68830003)
-                          (.replicaVersion 5000000000))
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              bs ^bytes (messaging-compress {:a nil :b nil})
-              cnt ^int (alength bs)
-              segments-encoder (-> (.segmentsCount encoder 18)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt)
-                                   (.putSegmentBytes bs 0 cnt))]
-          (.encodedLength encoder)
-          ))
-  ;(into [] buffer)
-  ))
-
+         {:replica-version replica-version 
+          :dest-id dest-id 
+          :segments (persistent! messages)})))))
